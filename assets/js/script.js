ff4b62c6778521ccd9b7ffc3c9f813a6bc90cb03
michaeloc1 <michaeloc@verizon.net>
2023-01-30 15:20:30 -0500

    added comments to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,32 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-//console.log("WINDOW-2: ", this)
-//console.log("DOM: ", document)
 
 $(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+//will retrieve  events scheduled and display them in the textarea of the
+//correct hour I made a unique key for local storage that consists of
+//the current day month and year. This way each days events are stored
+//separately.  One value in the object stored is the hour of the event
+//and the other value stored is the event
   var storedEvents = [];
- // if(localStorage.getItem("schedule") != null){
-    //storedEvents = JSON.parse(localStorage.getItem("schedule"));
-    var dateOfSchedule = dayjs().format('MMDDYY')
-      if(localStorage.getItem(dateOfSchedule) != null){
-    storedEvents = JSON.parse(localStorage.getItem(dateOfSchedule));
-    for(i = 0; i < storedEvents.length; i++){
-        console.log(storedEvents[i].time)
-        var testStorage = $('#' + storedEvents[i].time)
-        console.log(testStorage)
-       // testStorage.children('textarea').val()storedEvents[i].textEvent;
-       testStorage.children('textarea').val(storedEvents[i].textEvent);
+  var dateOfSchedule = dayjs().format('MMDDYY');
+  if(localStorage.getItem(dateOfSchedule) != null){
+     storedEvents = JSON.parse(localStorage.getItem(dateOfSchedule));
+      for(i = 0; i < storedEvents.length; i++){
+        var testStorage = $('#' + storedEvents[i].time);
+        testStorage.children('textarea').val(storedEvents[i].textEvent);
     
     }
   }
-  console.log(storedEvents)
- // var dateOfSchedule = dayjs().format('MMDDYY')
-  console.log(dateOfSchedule)
 
-  var getBtn = $('.btn')
-      //console.log(getBtn)
+//button click event. I get the id of the parent div id and the sibling textare
+//value to save to local storage these are used as the keys to the object that
+//will be stored in localstorage. I use jquery grep so if any event for the 
+//hour exists it will be deleted and a new event appended. I add the new
+//object to the storedevents array and it is added to localstorage
+//the last thing I do is open a dialog box that tells user the event has
+//been saved
+  var getBtn = $('.btn');
       getBtn.on("click", function(){
-          var getParentID = ($(this).parent().attr('id'))
-          //console.log(getParentID);
-          var getTextArea = ($(this).siblings(".description").val())
-          console.log("HERE: ", getTextArea)
-          //localStorage.setItem(getParentID, getTextArea )
-
+          var getParentID = ($(this).parent().attr('id'));
+          var getTextArea = ($(this).siblings(".description").val());
           storedEvents = jQuery.grep(storedEvents , function (value) {
             return value.time != getParentID;
           });
@@ -50,92 +38,74 @@ $(document).ready(function () {
               }
           )
      
-          //localStorage.setItem("schedule", JSON.stringify(storedEvents))
-          localStorage.setItem(dateOfSchedule, JSON.stringify(storedEvents))
-          var getIdtoDisplay = $("#displayMessage")
-          getIdtoDisplay.text("Event stored in localstorage");
+          localStorage.setItem(dateOfSchedule, JSON.stringify(storedEvents));
+
           $('#dialog').dialog('open');
     })
 
 
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+//this sets the past present future classes on page load. I get the current
+//hour from dayjs then I get the divs that contain hour in the id and loop
+//thru them As it is being looped thru I get the full id and call a function
+//called getNumericPart that will remove all non numeric from the id and
+//return just the number which will be the hour. I then check to see if
+//the returned number is === to < or > and add the classes to the divs
+//I also disable the textarea and button ones that are in the past
 
-  var getHourPresent = parseInt(dayjs().format('H'))
-  //getHourPresent = getHourPresent - 5
+  var getHourPresent = parseInt(dayjs().format('H'));
   $('*[id*=hour]').each(function() {
-    var getDivID = $(this).attr("id")
-    console.log(storedEvents.time)
-  
-
+    var getDivID = $(this).attr("id");
     var getHourFromID = parseInt(getNumericPart(getDivID));
-      console.log(getHourFromID)
-      console.log(getHourPresent)
 
       if (getHourFromID === getHourPresent){
-        $(this).addClass('present')
+        $(this).addClass('present');
       } 
       else if (getHourFromID < getHourPresent){
-      $(this).addClass('past')
+      $(this).addClass('past');
       $(this).children("textarea").attr("disabled", true);
       $(this).children("button").prop('disabled', true);
     }
       else{
-        $(this).addClass('future')
+        $(this).addClass('future');
       }
 
  })
 
+//I got this function from https://gabrieleromanato.name/.  I use it to
+//remove non numeric characters from an ID and return just the numbers
     function getNumericPart(id){
       var $num = id.replace(/[^\d]+/, '');
       return $num;
     }
   //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-
-
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
-var displayTime = dayjs().format('dddd, MMMM D')
-var currentDayEl = $('#currentDay')
+ 
+//this displays the current date on the webpage
+var displayTime = dayjs().format('dddd, MMMM D YYYY');
+var currentDayEl = $('#currentDay');
 currentDayEl.text(displayTime);
 
 });
 
 
-
-//var varGetID = $('*[id*=' +  getHourPresent + ']')
-//console.log(varGetID)
-//varGetID.css('font-family', 'cursive')
-//varGetID.addClass('pickles')
-
-//setTimeout(function(){location.reload()},3600000 - ((new Date) % 3600000))
-
-  setTimeout(function(){alert("New Hour 1")},3600000 - ((new Date) % 3600000))
-
+//this is code for when the page has already opened and the hour changes
+//it will run on every new hour.  I get the present time and hour past and use
+//them to get the divs of the hours. I then remove classes and add classes
+//and disable the textarea and button of hour past
 setTimeout(function(){
- alert("New Hour2")
- var getHourPresent2 = parseInt(dayjs().format('H'))
- var getHourpast = getHourPresent2 - 1
-var varGetID = $('*[id*=' +  getHourPresent2 + ']')
-var varGetID2 =  $('*[id*=' +  getHourpast + ']')
-varGetID.removeClass('future')
-varGetID.addClass('present')
-varGetID2.removeClass('present')
-varGetID2.addClass('past');
-varGetID2.children("textarea").attr("disabled", true)
-varGetID2.children("button").prop('disabled', true);
+    var getHourPresent2 = parseInt(dayjs().format('H'));
+    var getHourpast = getHourPresent2 - 1;
+    var varGetID = $('*[id*=' +  getHourPresent2 + ']');
+    var varGetID2 =  $('*[id*=' +  getHourpast + ']');
+    varGetID.removeClass('future');
+    varGetID.addClass('present');
+    varGetID2.removeClass('present');
+    varGetID2.addClass('past');
+    varGetID2.children("textarea").attr("disabled", true);
+    varGetID2.children("button").prop('disabled', true);
 },3600000 - ((new Date) % 3600000))
 
-
+//this formats the dialog I have it set to false at start and will open
+//on button click
   $("#dialog").dialog({
       modal: true,
       autoOpen: false,
